3f847039d8378ac8f0fa46a25676d23881345953
tttdddg <2091445482@qq.com>
2026-02-23 18:37:19 +0800

    新增菜品接口开发

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,43 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO){
+        Dish dish=new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+
+        //向菜品表插入1条数据
+        dishMapper.insert(dish);
+
+        //获取插入生成的主键值id
+        Long dishId=dish.getId(); //把id赋给dishId
+
+        List<DishFlavor> flavors=dishDTO.getFlavors();
+        if(flavors!=null&&flavors.size()>0){
+            flavors.forEach(dishFlavor -> dishFlavor.setDishId(dishId));
+            //向口味表插入n条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
