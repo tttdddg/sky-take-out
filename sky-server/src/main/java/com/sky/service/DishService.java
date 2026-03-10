b2a15697c414636e1b685aedeb4f4f65e96e0e32
tttdddg <2091445482@qq.com>
2026-03-10 12:08:24 +0800

    小程序商品浏览：  	1. 查询分类接口  	2. 根据分类 id 查询菜品接口  	3. 根据分类 id 查询套餐接口  	4. 根据套餐 id 查询包含的菜品接口

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 import org.springframework.stereotype.Service;
@@ -25,4 +26,11 @@ public interface DishService {
 
     //修改菜品
     void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
