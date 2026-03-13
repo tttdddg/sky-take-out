0b7b4bdc7537c25e8ef374f83064ef932b6f9308
tttdddg <2091445482@qq.com>
2026-03-14 03:02:50 +0800

    添加购物车接口开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,10 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ShoppingCartService {
+    //添加购物车
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
